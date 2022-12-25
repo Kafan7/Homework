9a9b1c7726645df1e2481b1c4d7f3f2218b5dd53
Vladislav K <mckafan@yandex.ru>
2022-12-25 18:57:13 +0300

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    TableSquare(number);
+}
+else Console.WriteLine("Не натуральное число");
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 4} {Math.Pow(count, 2), 4}");
+        count++;
+    }
+}
