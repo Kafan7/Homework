f2327fcaf687baa2ebe30a7ffae46f53d82b6fa8
Vladislav K <mckafan@yandex.ru>
2022-12-25 17:48:33 +0300

    Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите номер четверти ");
+
+string quarter = Console.ReadLine();
+string result = QuarterCoordinates(quarter);
+Console.WriteLine(result);
+
+string QuarterCoordinates(string number);
+{
+    if (number == "1") return "x > 0; y > 0";
+    if (number == "2") return "x < 0; y > 0";
+    if (number == "3") return "x < 0; y < 0";
+    if (number == "4") return "x > 0; y < 0";
+    return "Неверный номер четверти";
+}
+
+
+
+
+
+
+
