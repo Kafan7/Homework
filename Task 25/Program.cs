83f2ed18ed2a852a74952ee497f1a95a4c468bb5
Vladislav K <mckafan@yandex.ru>
2023-01-16 19:38:20 +0300

    Task 25

---
+++ Task 25/Program.cs
@@ -0,0 +1,21 @@
+﻿ /*Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.*/
+
+Console.Write("Введите число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите  число B: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int multiplicationNumbers = MultiplicationNumbers(number, number1);
+Console.WriteLine($"Число {number} в степени {number1} = {multiplicationNumbers} ");
+
+int MultiplicationNumbers(int num, int num1)
+{
+    int multiplication = num;
+    for (int i = 2; i <= num1; i++)
+    {
+        multiplication *= num;
+    }
+    return multiplication;
+}
+
