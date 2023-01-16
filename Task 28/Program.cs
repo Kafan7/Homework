e5bdeb272d16f279eb824520765488318915bcf5
Vladislav K <mckafan@yandex.ru>
2023-01-16 16:58:28 +0300

    Task 26

---
+++ Task 28/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 28: Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Произвидение числа {number}  = {sumNumbers} ");
+
+int SumNumbers(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum *= i;
+        }
+    }
+    return sum;
+}
