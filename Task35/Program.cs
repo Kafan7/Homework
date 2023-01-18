82ef3209173f1f1d2ed40df4fd6a0ff1f78021e3
Vladislav K <mckafan@yandex.ru>
2023-01-18 16:57:46 +0300

    Task 35

---
+++ Task35/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] array = CreatArrayRndInt(11, -100, 100);
+PrintArray(array);
+int countElements = CountElements(array);
+
+Console.WriteLine(countElements);
+
+int[] CreatArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int CountElements(int[] arr, int min = 10, int max = 99)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= min && arr[i] <= max) count++;
+    }
+    return count;
+}
