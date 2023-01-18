f85f0dcdb423edb6bac859f3e2fbb6b0a4c899a1
Vladislav K <mckafan@yandex.ru>
2023-01-18 18:54:54 +0300

    Task 34

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -20,22 +20,7 @@ void PrintArray(int[] arr)
     Console.Write("]");
 }
 
-int CountElements(int[] arr)
-{
-    int size = arr.Length / 2;
-    if (arr.Length % 2 == 1) size += 1;
-    int[] newArr = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        if (i != arr.Length - 1 - i)
-        newArr[i] = arr[i] * arr[arr.Length - 1 - i];
-        else newArr[i] = arr[i];
-    }
-    return newArr;
-}
+
 
 int[] array = CreatArray(11, -100, 100);
 PrintArray(array);
-int countElements = CountElements(array);
-
-Console.WriteLine(countElements);
