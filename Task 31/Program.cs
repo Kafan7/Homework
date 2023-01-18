4d70417649a7892c0a979555df41fe320794b368
Vladislav K <mckafan@yandex.ru>
2023-01-18 15:52:55 +0300

    Task 31

---
+++ Task 31/Program.cs
@@ -0,0 +1,49 @@
+﻿int[] CreatArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int GetSumPositivElement(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int GetSumNegativElement(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreatArrayRndInt(12, -9, 9);
+PrintArray(array);
+int sumPositiv = GetSumPositivElement(array);
+int sumNegativ = GetSumNegativElement(array);
+
+Console.WriteLine($"Сумма положительных элементов = {sumPositiv}");
+Console.WriteLine($"Сумма отрицательных элементов = {sumNegativ}");
