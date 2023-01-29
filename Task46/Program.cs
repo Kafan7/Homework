353b596de8d216a3c566655f2b49d4d2c501f850
Vladislav K <mckafan@yandex.ru>
2023-01-29 18:10:22 +0300

    Task 47

---
+++ Task46/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 46: Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1*/
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); i++)
+    {
+        matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    }
+    return matrix;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
---
+++ Task47/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 47: Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.*/
+
+double[,] CreateRandom2dArray()
+{
+ Console.Write("Введите количество строк: ");
+ int rows = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите количество столбцов: ");
+ int columns = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Ввод минимального значения: ");
+ int minValue = Convert.ToInt32(Console.ReadLine());
+    
+ Console.Write("Ввод максимального значения: ");
+ int maxValue = Convert.ToInt32(Console.ReadLine());
+ double[,] array= new double[rows, columns];
+ for(int i = 0; i< rows;i++)
+ for(int j= 0; j < columns; j++)
+ array[i,j] = Math.Round((new Random().Next(minValue, maxValue) + new Random().NextDouble()), 2);
+ return array;
+}
+void Show2dArray(double[,] array)
+{
+ for(int i = 0; i < array.GetLength(0); i++)
+      {
+ for(int j = 0; j < array.GetLength(1); j++)
+ Console.Write(array[i, j] + " ");
+ Console.WriteLine();
+      }
+      Console.WriteLine();
+}
+double[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
